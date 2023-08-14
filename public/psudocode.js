34fd16d2e23736cadbff9c2cdbe362cc4aa2f5ba
RobertC91 <134195467+RobertC91@users.noreply.github.com>
2023-08-14 12:45:48 -0500

    imported starter code

---
+++ public/psudocode.js
@@ -0,0 +1,6 @@
+// bonus: delete notes
+// implement some form of uuid for the different notes
+// create html routes for /api/notes that will serve up a notes.html file
+// create wildcard route that will serve up the index.html
+// create endpoints for api/notes to get and post new notes
+// deploy using heroku
